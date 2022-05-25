e851f944bf6dde0da7cd914444bc475b8d5e229c
tushar5526 <codingid6@gmail.com>
2022-05-25 23:59:29 +0530

    Updates

---
+++ precache-manifest.87be2af9a8e14da650dcda713f1962eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587e678110e1312cd68484434d002e6f",
+    "url": "/Giphy-Assignment/index.html"
+  },
+  {
+    "revision": "5f1aa710df005313b901",
+    "url": "/Giphy-Assignment/static/css/main.643f910e.chunk.css"
+  },
+  {
+    "revision": "fc74cd78a172ba30e6ad",
+    "url": "/Giphy-Assignment/static/js/2.5c4e98d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Giphy-Assignment/static/js/2.5c4e98d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1aa710df005313b901",
+    "url": "/Giphy-Assignment/static/js/main.8610ddde.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Giphy-Assignment/static/js/main.8610ddde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f376371cac3b441a26c1",
+    "url": "/Giphy-Assignment/static/js/runtime-main.aaf06b04.js"
+  }
+]);
