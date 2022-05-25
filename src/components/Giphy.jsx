c6fe5a9fb5b3ee84508e753f37120e2f14d6c7cf
tushar5526 <codingid6@gmail.com>
2022-05-25 21:37:40 +0530

    Added a simple UI to fetch, render and load gifs

---
+++ src/components/Giphy.jsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import Loader from "./Loader";
+
+const Giphy = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(25);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+          params: {
+            api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
+            limit: 100
+          }
+        });
+
+        console.log(results);
+        setData(results.data.data);
+      } catch (err) {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 4000);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderGifs = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+    return currentItems.map(el => {
+      return (
+        <div key={el.id} className="gif">
+          <img src={el.images.fixed_height.url} alt="gif" />
+        </div>
+      );
+    });
+  };
+  const renderError = () => {
+    if (isError) {
+      return (
+        <div
+          className="alert alert-danger alert-dismissible fade show"
+          role="alert"
+        >
+          Unable to get Gifs, please try again in a few minutes
+        </div>
+      );
+    }
+  };
+
+  const handleSearchChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const results = await axios("https://api.giphy.com/v1/gifs/search", {
+        params: {
+          api_key: "GlVGYHkr3WSBnllca54iNt0yFbjz7L65",
+          q: search,
+          limit: 100
+        }
+      });
+      setData(results.data.data);
+    } catch (err) {
+      setIsError(true);
+      setTimeout(() => setIsError(false), 4000);
+    }
+
+    setIsLoading(false);
+  };
+
+  const pageSelected = pageNumber => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div className="m-2">
+      {renderError()}
+      <form className="form-inline justify-content-center m-2">
+        <input
+          value={search}
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="search"
+          className="form-control"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="btn btn-primary mx-2"
+        >
+          Go
+        </button>
+      </form>
+      <div className="container gifs">{renderGifs()}</div>
+    </div>
+  );
+};
+
+export default Giphy;
